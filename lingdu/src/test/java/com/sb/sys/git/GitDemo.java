03e99d0a7e1ff5eb8e32db96cd273d9ec5f1665c
ksana404 <ksana404@163.com>
2020-11-04 18:31:46 +0800

    lingdu01:Git第一次提交,修改yml文件,新增test/git包.

---
+++ lingdu/src/test/java/com/sb/sys/git/GitDemo.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C), 2015-2020, XXX有限公司
+ * FileName: GitDemo
+ * Author:   Administrator
+ * Date:     2020/11/4 0004 18:28
+ * Description: git测试
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.sb.sys.git;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈git测试〉
+ *
+ * @author Administrator
+ * @create 2020/11/4 0004
+ * @since 1.0.0
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        //IDEA git修改提交测试
+        System.out.println("IDEA git修改提交测试");
+    }
+
+}
