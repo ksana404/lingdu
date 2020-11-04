6f62500835e1ec699998b9e26e61ecc62b391290
ksana404 <ksana404@163.com>
2020-11-04 18:40:30 +0800

    lingdu提交到本地缓存:修改yml,新增test/git包.

---
+++ lingdu/src/test/java/com/sb/sys/git/GitTestA.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C), 2015-2020, XXX有限公司
+ * FileName: GitTestA
+ * Author:   Administrator
+ * Date:     2020/11/4 0004 13:20
+ * Description: git测试
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.sb.sys.git;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈git测试〉
+ *
+ * @author Administrator
+ * @create 2020/11/4 0004
+ * @since 1.0.0
+ */
+public class GitTestA {
+    //从IDEA新建的git 修改配置文件 新增包 提交到master
+    public static void main(String[] args) {
+        System.out.println("从IDEA新建的git");
+    }
+
+}
