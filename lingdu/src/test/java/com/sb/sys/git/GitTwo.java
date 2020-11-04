20fde8b44ad4c56f2903ad6df4483c78831ed6c3
ksana404 <ksana404@163.com>
2020-11-04 18:43:43 +0800

    lingdu提交到远程:二次提交,新增GitTwo.

---
+++ lingdu/src/test/java/com/sb/sys/git/GitTwo.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C), 2015-2020, XXX有限公司
+ * FileName: GitTwo
+ * Author:   Administrator
+ * Date:     2020/11/4 0004 18:41
+ * Description: 二次测试
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.sb.sys.git;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈二次测试〉
+ *
+ * @author Administrator
+ * @create 2020/11/4 0004
+ * @since 1.0.0
+ */
+public class GitTwo {
+    public static void main(String[] args) {
+        System.out.println("二次提交");
+    }
+
+}
